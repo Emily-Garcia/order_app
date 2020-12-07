73a1b94b0b90c7fabe4e36af48cb26f19dbabe01
Emily Garcia <garcia.16.emily@gmail.com>
2020-12-07 10:09:47 -0600

    Agregar un platillo

--- order_app.py
+++ order_app.py
@@ -13,7 +13,7 @@ def create_table_dish(conex):
     sql = '''
         CREATE TABLE IF NOT EXISTS dish (
             name VARCHAR NOT NULL,
-            descripcion TEXT NOT NULL,
+            description TEXT NOT NULL,
             price DOUBLE NOT NULL,
             is_available INT NOT NULL,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -29,9 +29,10 @@ def principal_menu(conex):
     print('1 - Crear platillo')
     print('2 - Ver platillos')
     print('3 - Actualizar platillo')
-    print('3 - Eliminar platillo')
-    option = int(input('Ingrese la opcion de lo que quiera realizar: '))
-    if validate_user_selection(option, 3):
+    print('4 - Eliminar platillo')
+    print('5 - Salir del programa')
+    option = int(input('Ingrese la opción de lo que quiera realizar: '))
+    if validate_user_selection(option):
         selection_principal(option, conex)
     else:
         print('El valor que ingresaste no es valido')
@@ -40,15 +41,41 @@ def principal_menu(conex):
 # Metodo para llevar a cada opcion del menu principal
 def selection_principal(option, conex):
     if option == 1:
-        pass
+        create_dish(conex)
     elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
         pass
     else:
         pass
 
 # Metodo para validar la opcion
-def validate_user_selection(option, options):
-    return isinstance(option, int) and option > 0 and option <= options
+def validate_user_selection(option):
+    return isinstance(option, int) and option > 0 and option <=5
+
+# Metodo para crear un platillo
+def create_dish(conex):
+    print('--- CREAR PLATILLO ---')
+    name = input('Ingresa el nombre: ')
+    description = input('Ingresa una descripción: ')
+    price = input('Ingresa el precio: ')
+    is_available = input('¿Cuántos platillos estan disponibles?: ')
+
+    sql = '''
+        INSERT INTO
+        dish (name, description, price, is_available)
+        VALUES (?, ?, ?, ?)
+    '''
+    values = (name, description, price, is_available)
+
+    conex.execute(sql, values)
+    conex.commit()
+
+    print('Usuario creado correctamete!')
+    print('-----------------------------')
+    principal_menu(conex)
 
 # Metodo principal
 def main():
